1f178e77f01b2f1eaa838018625aec33dd9264a8
Bitpatroon <github@bitpatroon.nl>
2021-05-11 12:10:31 +0200

    Fix (reused) id

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@ if (!defined('TYPO3_MODE')) {
 call_user_func(
     function () {
         if (TYPO3_MODE === 'BE') {
-            $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1619622485] = [
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1620726495] = [
                 'nodeName' => 'variableTextControl',
                 'priority' => 90,
                 'class'    => \BPN\BpnVariableText\Backend\UserFunction\VariableText::class,
